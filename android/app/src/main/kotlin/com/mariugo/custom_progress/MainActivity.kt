0f6c66766ee180782e60374aa5b38250165a11b7
Mariugo Megiani <mariugomegiani@gmail.com>
2022-11-11 06:03:50 -0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/mariugo/custom_progress/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.mariugo.custom_progress
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/custom_progress.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'dart:math' as math;
+
+class CustomProgress extends StatefulWidget {
+  const CustomProgress({super.key});
+
+  @override
+  State<CustomProgress> createState() => _CustomProgressState();
+}
+
+class _CustomProgressState extends State<CustomProgress> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
